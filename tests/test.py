cc3f6204c1099b77d9c5fae34323741e396a39b1
Jan Knedlik <j.knedlik@gsi.de>
2021-05-31 08:16:59 +0200

    Added simple tests for python

--- tests/test.py
+++ tests/test.py
@@ -4,3 +4,9 @@ import pyneva as m
 assert m.__version__ == "0.0.1"
 assert m.add(1, 2) == 3
 assert m.subtract(1, 2) == -1
+A=m.EA;
+print(dir(A.config))
+print(A.config)
+print(A.config)
+x=A.config
+print(x)
