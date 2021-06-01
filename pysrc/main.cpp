13f54409e7b1521eb1eed2ba07ea11921ac45d02
Jan Knedlik <j.knedlik@gsi.de>
2021-06-01 14:20:34 +0200

    Finished optimize wrapper and edited tests, * add some comments

--- pysrc/main.cpp
+++ pysrc/main.cpp
@@ -39,38 +39,46 @@ PYBIND11_MODULE(pyneva, m)
 
         Some other explanation about the subtract function.
     )pbdoc");
+  // custom wrapper for the Go3 EA Algorithm
   py::class_<GO3::Algorithm_EA>(m, "EA")
       .def(py::init<int>())  // Algorithm ctor
       .def_readwrite("config",
-		     &GO3::Algorithm_EA::cfg);	// access to attributes
-  // custom wrapper for Population Wrapper
-  using ftype = std::function<double(std::vector<double>)>;
+		     &GO3::Algorithm_EA::cfg);	// access to cfg attributes
   using dvec = std::vector<double>;
+  using ftype = std::function<double(std::vector<double>)>;
+  // custom wrapper for the Go3 Population
   py::class_<GO3::Population<ftype>>(m, "Population")
-      .def(py::init([](int size, int numParents) {
-	return GO3::Population<ftype>(
-	    {5, 5, 5}, {0, 0, 0}, {10, 10, 10},
-	    [](std::vector<double> v) {
-	      return std::accumulate(v.begin(), v.end(), 0);
-	    },
-	    size, numParents);
-      }));
+      .def(py::init(  // Population ctor
+	  [](int size, int numParents) {
+	    return GO3::Population<ftype>(
+		{5, 5, 5}, {0, 0, 0}, {10, 10, 10},
+		[](std::vector<double> v) {
+		  return std::accumulate(v.begin(), v.end(), 0);
+		},
+		size, numParents);
+	  }));
+  // custom wrapper for the Go3 Optimizer
   py::class_<GO3::GenevaOptimizer3>(m, "GOptimizer")
-      //.def(py::init([](py::list argv_list) {
-      .def(py::init([](std::vector<std::string> argv_str) {
-	// std::vector<std::string> argv_str;  // argv using strings
-	// copy into
-	// for (py::handle obj : argv_list)
-	//  argv_str.push_back(obj.attr("__str__")().cast<std::string>());
-	// argv as char* using that underlying memory
-	std::vector<char*> argv(argv_str.size());
-	std::generate(argv.begin(), argv.end(), [n = 0, &argv_str]() mutable {
-	  // evil const cast magic that should be changed in GenevaOptimizer
-	  return const_cast<char*>(argv_str[n++].c_str());
-	});
-	return std::make_unique<GO3::GenevaOptimizer3>(argv.size(),
-						       argv.data());
-      }));
+      .def(py::init(  // Optimizer ctor
+	  [](std::vector<std::string> argv_str) {
+	    std::vector<char*> argv(argv_str.size());
+	    std::generate(argv.begin(), argv.end(),
+			  [n = 0, &argv_str]() mutable {
+			    // evil const cast magic that should be changed in
+			    // GenevaOptimizer
+			    return const_cast<char*>(argv_str[n++].c_str());
+			  });
+	    // use unique_ptr as the go2 attribute of go3 is not moveable
+	    return std::make_unique<GO3::GenevaOptimizer3>(argv.size(),
+							   argv.data());
+	  }))
+      .def("optimize",
+	   [](GO3::GenevaOptimizer3& self, GO3::Population<ftype>& pop,
+	      GO3::algorithmsT algos) {
+	     return (*self.optimize(pop, algos)).raw_fitness();
+	   })
+
+      ;
 
 #ifdef VERSION_INFO
   m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
--- tests/test.py
+++ tests/test.py
@@ -5,8 +5,10 @@ assert m.__version__ == "0.0.1"
 assert m.add(1, 2) == 3
 assert m.subtract(1, 2) == -1
 A=m.EA(10);
+B=m.EA(20);
 print(dir(A.config))
 print(A.config[1])
 x=A.config
-pop=m.Population(10,5)
-go3=m.GOptimizer(["--client"])
+pop=m.Population(20,2)
+go3=m.GOptimizer(["-c","sc"])
+print(go3.optimize(pop,[A,B]))
